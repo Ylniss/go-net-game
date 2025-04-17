6b96b355d6bb0bdc276677d8794c77457a8e9beb
Ylniss <zupqa0@gmail.com>
2025-04-17 21:00:07 +0200

    wip menu

--- client/main.go
+++ client/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
 
-	"github.com/TwiN/go-color"
-	"github.com/cqroot/prompt"
-	"github.com/cqroot/prompt/input"
+	// "github.com/TwiN/go-color"
+	// "github.com/cqroot/prompt"
+	// "github.com/cqroot/prompt/input"
 	"github.com/eiannone/keyboard"
 
-	"github.com/ylniss/go-net-game/msg"
+	"github.com/ylniss/go-net-game/messaging"
 )
 
 func main() {
@@ -23,7 +24,7 @@ func main() {
 	defer conn.Close()
 
 	// diaplay menu and open keyboard read connection
-	fmt.Printf("[c] - connect to a new room%t[q] - quit\n\n")
+	fmt.Println("[c] - connect to a new room\t[q] - quit")
 
 	if err := keyboard.Open(); err != nil {
 		panic(err)
@@ -39,12 +40,15 @@ func main() {
 		}
 
 		if char == 'c' {
-            err := sendMessageToServer(&msg.ClientMessage{Event: "connect_to_room", EventType: "menu", Data: ""})
-            if err != nil {
-                fmt.Println(err.Error())
-                continue
-            }
-            
+			err := sendMessageToServer(conn, &msg.ClientMessage{
+				Event:     "connect_to_room",
+				EventType: "menu",
+				Data:      "",
+			})
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
 
 		} else if char == 'q' {
 			os.Exit(0) // quit client with success
@@ -58,17 +62,19 @@ func main() {
 	// 	}
 	// 	return "", err
 	// }
-
 }
 
-func sendMessageToServer(msg *msg.ClientMessage) : err {
-	_, err = conn.Write([]byte(msg))
+func sendMessageToServer(conn net.Conn, m *msg.ClientMessage) error {
+	// 1) Marshal the struct to JSON
+	data, err := json.Marshal(m)
 	if err != nil {
-		fmt.Println("Error writing to connection:", err)
-		return err
+		return fmt.Errorf("failed to marshal message: %w", err)
 	}
 
-    return _
-	// fmt.Println("Sent message to server:", message)
-    
+	// 2) Write the JSON
+	if _, err := conn.Write(data); err != nil {
+		return fmt.Errorf("error writing to connection: %w", err)
+	}
+
+	return nil
 }
